3450c4445e74b0eec7b32c0761570577c4312f09
lan <jhj4n1m@qq.com>
2019-07-30 20:21:46 +0800

    zijiduanxin

--- SwiperSocial/SwiperSocial/config.py
+++ SwiperSocial/SwiperSocial/config.py
@@ -2,10 +2,10 @@
 
 YZX_SMS_API = 'https://open.ucpaas.com/ol/sms/sendsms'
 YZX_SMS_PARAMS = {
-    "sid": 'a88108ca93a370050637e49ec1c905de',
-    "token": '730223d4cd49a0169ba5141e6d208cc6',
-    "appid": '44b8828cb3aa408e9b8b74059d6f2f1b',
-    "templateid": "487820",
+    "sid": '118a70b9378fa6dfddeb0518b7e5ca31',
+    "token": 'faf0b16e7daa42921f929c75b234547c',
+    "appid": '66b9c8538df94d518d7ac46a250949ea',
+    "templateid": "489731",
     "param": None,
     "mobile": None,
 }
